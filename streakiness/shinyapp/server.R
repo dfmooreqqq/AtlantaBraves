439c8a7416153da10650b3c1ab0958ded77dcc13
Daniel Moore <dfmoore@gmail.com>
2014-09-18 06:23:52 -0700

    started shiny App

---
+++ streakiness/shinyapp/server.R
@@ -0,0 +1,8 @@
+library(shiny)
+
+# Let's load the full season csv from the github depository
+
+# Define server logic required to plot various variables against mpg
+shinyServer(function(input, output) {
+    
+})
